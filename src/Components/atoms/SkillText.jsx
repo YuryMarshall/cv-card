c799054ff29154e3bd5a24799f70444f85bef58d
Kalashnik Yury <msgkalashnik@gmail.com>
2023-04-06 13:04:55 +0300

    reworked skills section

--- src/Components/atoms/SkillText.jsx
+++ src/Components/atoms/SkillText.jsx
@@ -3,10 +3,11 @@ import styles from "../organisms/styles/skills.module.css";
 import skillsData from "../data/skillsData";
 
 function SkillText(props) {
+  const LangCheck = () => (props.language ? props.language : "eng");
   return (
     <p className={styles.skill__text}>
       {props.skill
-        ? skillsData[props.skill][props.language]
+        ? skillsData[props.skill][LangCheck()]
         : props.language === "eng"
         ? "Please select skill of developer upper."
         : "Пожалуйста, выберете навык разработчика."}
